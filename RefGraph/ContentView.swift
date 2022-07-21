b07f7245c48a54d0c67753e3f8e1884c7cb5f930
m salman a <mohammadalfarisi6@gmail.com>
2022-07-21 11:38:19 +0700

    Initial Commit

---
+++ RefGraph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RefGraph
+//
+//  Created by Mohammad Alfarisi on 21/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RefGraph/RefGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefGraphApp.swift
+//  RefGraph
+//
+//  Created by Mohammad Alfarisi on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RefGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
